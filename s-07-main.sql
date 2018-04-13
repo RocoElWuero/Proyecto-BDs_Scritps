00270479f70e02df4b5ba3c47a0d15c499a0a809
RojasCastilloOscar-RocoElWuero <36763059+RojasCastilloOscar-RocoElWuero@users.noreply.github.com>
2018-04-13 12:39:52 -0500

    Add files via upload
    Primera version del Main

---
+++ s-07-main.sql
@@ -0,0 +1,18 @@
+Prompt Caso ventas de seguros
+
+Prompt Cargando usuarios ....
+@s-01-usuarios.sql
+
+Prompt Cargando secuencias ...
+@s-05-secuencias.sql
+
+Prompt Cargando tablas ...
+@s-02-entidades.sql
+
+Prompt Cargando tablas temporales ...
+@s-03-tablas-temporales.sql
+
+Prompt Realizando consultas ...
+@s-08-consultas.sql
+
+Prompt Listo.
