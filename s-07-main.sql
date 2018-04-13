86ac1df4334d227a77cb5a312ea0f13cc8073c64
RojasCastilloOscar-RocoElWuero <36763059+RojasCastilloOscar-RocoElWuero@users.noreply.github.com>
2018-04-13 13:04:49 -0500

    Add files via upload

--- s-07-main.sql
+++ s-07-main.sql
@@ -3,6 +3,11 @@ Prompt Caso ventas de seguros
 Prompt Cargando usuarios ....
 @s-01-usuarios.sql
 
+
+Prompt Cargando usuario administrador
+-- Para autologearme como el usuario
+connect &J_PROY_ADMIN as sysdba
+
 Prompt Cargando secuencias ...
 @s-05-secuencias.sql
 
@@ -11,6 +16,10 @@ Prompt Cargando tablas ...
 
 Prompt Cargando tablas temporales ...
 @s-03-tablas-temporales.sql
+
+Prompt Cargando usuario administrador
+-- Para autologearme como el usuario
+connect &R_PROY_INVITADO/&orcinvitadopass
 
 Prompt Realizando consultas ...
 @s-08-consultas.sql
